2a67f35bff45587dd87e068335ef222694c10474
ahshasib <154332574+ahshasib@users.noreply.github.com>
2025-09-03 08:02:07 +0600

    initial

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export', // এটি বলে যে site static export হবে
+    distDir: 'out',   // export হওয়া folder
+  }
+  
+  module.exports = nextConfig
+  
