24498fd5c93277c9451a5272146996ff33495894
ahshasib <154332574+ahshasib@users.noreply.github.com>
2025-09-03 08:06:25 +0600

    change some file

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export', // এটি বলে যে site static export হবে
-    distDir: 'out',   // export হওয়া folder
-  }
-  
-  module.exports = nextConfig
-  
+const nextConfig = {};
+
+export default nextConfig;
